3ff664fe9f5d592323fc0237ca27d7defdca2f42
charleshadden <charles.r.hadden@gmail.com>
2018-07-11 11:48:08 -0500

    refactored map

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,32 @@
 "use strict";
 (function () {
 
+//===========jquery styling========\\
+
+    $('.box').hover(function() {
+        $(this).css('font-style','italic');
+    }, (function() {
+        $(this).css('font-style','normal')
+    }));
+
+    $('#nightmode').hover(function() {
+        $(this).css('background-color','black');
+        $(this).css('color','white')
+    }, (function() {
+        $(this).css('background-color','white');
+        $(this).css('color','black')
+    }));
+
+    $('#daymode').hover(function() {
+        $(this).css('background-color','white');
+        $(this).css('color','black')
+    }, (function() {
+        $(this).css('background-color','black');
+        $(this).css('color','white')
+    }));
+
+//==========daymode map=========\\
+
     var map;
     var lat = 29.4241;
     var lng = -98.4936;
@@ -12,6 +38,152 @@
         map = new google.maps.Map(document.getElementById('map'), {
             zoom: 3,
             center: myLatLng
+
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            label: 'Drag',
+            draggable: true
+        });
+
+        marker.addListener('mouseup', function() {
+            var lat = marker.getPosition().lat();
+            var lng = marker.getPosition().lng();
+            myLatLng.lat = lat;
+            myLatLng.lng = lng;
+            weatherUpdate(lat, lng);
+        });
+    }
+
+//=========nightmode map========\\
+
+    $('#nightmode').click(function() {
+        $('#nightmode').hide();
+        $('#daymode').show();
+        $('#map').toggle().hide();
+        $('#map2').toggle().show();
+        $('body').css('background-color','black', 'color','white');
+        $('h1').css('color','white');
+        $('#city').css('color','white');
+        $('.box').css('background-color','black');
+        $('.box').css('color','white');
+        $('.box').css('border','2px solid white');
+
+        initMap2()
+    });
+
+//=========daymode=========\\
+
+    $('#daymode').click(function(){
+        $('#nightmode').show();
+        $('#daymode').hide();
+        $('#map').toggle().show();
+        $('#map2').toggle().hide();
+        $('body').css('background-color','antiquewhite', 'color','black');
+        $('h1').css('color','black');
+        $('#city').css('color','black');
+        $('.box').css('background-color','#cbffcd');
+        $('.box').css('color','black');
+        $('.box').css('border','2px solid black');
+        $('#map').css('border', '2px solid black');
+
+        initMap()
+    });
+
+    initMap2();
+
+//============nightmode map==========\\
+
+    function initMap2() {
+
+        var myLatLng = {lat: lat, lng: lng};
+
+        map = new google.maps.Map(document.getElementById('map2'), {
+            zoom: 3,
+            center: myLatLng,
+            styles: [
+                {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+                {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+                {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+                {
+                    featureType: 'administrative.locality',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#d59563'}]
+                },
+                {
+                    featureType: 'poi',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#d59563'}]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'geometry',
+                    stylers: [{color: '#263c3f'}]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#6b9a76'}]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry',
+                    stylers: [{color: '#38414e'}]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry.stroke',
+                    stylers: [{color: '#212a37'}]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#9ca5b3'}]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [{color: '#746855'}]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry.stroke',
+                    stylers: [{color: '#1f2835'}]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#f3d19c'}]
+                },
+                {
+                    featureType: 'transit',
+                    elementType: 'geometry',
+                    stylers: [{color: '#2f3948'}]
+                },
+                {
+                    featureType: 'transit.station',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#d59563'}]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'geometry',
+                    stylers: [{color: '#17263c'}]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#515c6d'}]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.stroke',
+                    stylers: [{color: '#17263c'}]
+                }
+            ]
+
         });
 
         var marker = new google.maps.Marker({
@@ -29,6 +201,8 @@
         });
     }
 
+//===============updates weather based on movement of marker=======\\
+
     function weatherUpdate(lat, lng) {
 
         $.get("http://api.openweathermap.org/data/2.5/forecast", {
@@ -38,40 +212,60 @@
             units: "imperial",
             main: ""
         }).done(function(data) {
+            console.log(data);
 
-            $('#temp').html("<strong>Temp: </strong> " + Math.round(data.list[0].main.temp_max) + "\xB0" + " / " + Math.round(data.list[0].main.temp_min) + "\xB0");
-            $('#cloud').html("<strong>Weather: </strong> " + data.list[0].weather[0].description);
-            $('#humidity').html("<strong>Humidity: </strong> " + data.list[0].main.humidity);
-            $('#wind').html("<strong>Wind: </strong> " + data.list[0].wind.speed);
-            $('#pressure').html("<strong> Pressure: </strong>" + data.list[0].main.pressure);
+//================generates data==============\\
 
-            $('#temp2').html("<strong>Temp: </strong> " + Math.round(data.list[8].main.temp_max) + "\xB0" + " / " + Math.round(data.list[0].main.temp_min) + "\xB0");
-            $('#cloud2').html("<strong>Weather: </strong> " + data.list[8].weather[0].description);
-            $('#humidity2').html("<strong>Humidity: </strong> " + data.list[8].main.humidity);
-            $('#wind2').html("<strong>Wind: </strong> " + data.list[8].wind.speed);
-            $('#pressure2').html("<strong>Pressure: </strong>" + data.list[8].main.pressure);
+            function buildList1(data) {
+                var output = "";
 
-            $('#temp3').html("<strong>Temp: </strong> " + Math.round(data.list[16].main.temp_max) + "\xB0" + " / " + Math.round(data.list[0].main.temp_min) +  "\xB0");
-            $('#cloud3').html("<strong>Weather: </strong> " + data.list[16].weather[0].description);
-            $('#humidity3').html("<strong>Humidity: </strong> " + data.list[16].main.humidity);
-            $('#wind3').html("<strong>Wind: </strong> " + data.list[16].wind.speed);
-            $('#pressure3').html("<strong>Pressure: </strong> " + data.list[16].main.pressure);
+                output += "<h4>" + 'Today'  +"</h4>";
+                output += "<p>" + "<strong> Temp: </strong>" +  Math.round(data.list[0].main.temp_max) + "\xB0" + " / " + Math.round(data.list[0].main.temp_min) +"\xB0"+ "</p>";
+                output += "<p>" + "<img src='http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png'>" + "</p>";
+                output += "<p>" + "<strong> Weather: </strong>" + data.list[0].weather[0].description + "</p>";
+                output += "<p>" + "<strong> Humidity: </strong>" + data.list[0].main.humidity + "</p>";
+                output += "<p>" + "<strong> Wind: </strong>" + data.list[0].wind.speed + "</p>";
+                output += "<p>" + "<strong> Pressure: </strong>" + data.list[0].main.pressure; + "</p>";
 
-            $('#city').html(data.city.name + ", " +data.city.country);
+                return output;
+            }
+            $('#day1').html(buildList1(data));
 
-            var iconcode = data.list[0].weather[0].icon;
-            var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-            $('#wicon').attr('src', iconurl);
+            function buildList2(data) {
+                var output = "";
 
-            var iconcode2 = data.list[8].weather[0].icon;
-            var iconurl2 = "http://openweathermap.org/img/w/" + iconcode2 + ".png";
-            $('#wicon2').attr('src', iconurl2);
+                output += "<h4>" + 'Tomorrow'  +"</h4>";
+                output += "<p>" + "<strong> Temp: </strong>" +  Math.round(data.list[8].main.temp_max) + "\xB0" + " / " + Math.round(data.list[8].main.temp_min) +"\xB0"+ "</p>";
+                output += "<p>" + "<img class ='bounce' src='http://openweathermap.org/img/w/" + data.list[8].weather[0].icon + ".png'>" + "</p>";
+                output += "<p>" + "<strong> Weather: </strong>" + data.list[8].weather[0].description + "</p>";
+                output += "<p>" + "<strong> Humidity: </strong>" + data.list[8].main.humidity + "</p>";
+                output += "<p>" + "<strong> Wind: </strong>" + data.list[8].wind.speed + "</p>";
+                output += "<p>" + "<strong> Pressure: </strong>" + data.list[8].main.pressure + "</p>";
 
-            var iconcode3 = data.list[16].weather[0].icon;
-            var iconurl3 = "http://openweathermap.org/img/w/" + iconcode3 + ".png";
-            $('#wicon3').attr('src', iconurl3);
+                return output;
+            }
+            $('#day2').html(buildList2(data));
+
+            function buildList3(data) {
+                var output = "";
+
+                output += "<h4>" + 'Tomorrow'  +"</h4>";
+                output += "<p>" + "<strong> Temp: </strong>" +  Math.round(data.list[16].main.temp_max) + "\xB0" + " / " + Math.round(data.list[16].main.temp_min) +"\xB0"+ "</p>";
+                output += "<p>" + "<img src='http://openweathermap.org/img/w/" + data.list[16].weather[0].icon + ".png'>"+ "</p>";
+                output += "<p>" + "<strong> Weather: </strong>" + data.list[16].weather[0].description + "</p>";
+                output += "<p>" + "<strong> Humidity: </strong>" + data.list[16].main.humidity + "</p>";
+                output += "<p>" + "<strong> Wind: </strong>" + data.list[16].wind.speed + "</p>";
+                output += "<p>" + "<strong> Pressure: </strong>" + data.list[16].main.pressure + "</p>";
+
+                return output;
+            }
+            $('#day3').html(buildList3(data));
+
+            $('#city').html(data.city.name + ", " + data.city.country);
+
         });
     }
+
     initMap();
     weatherUpdate(lat, lng);
 
